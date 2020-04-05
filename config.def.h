1b2fe4da052dfd458b21288df86f87d5f1b40aef
chris5692 <52839899+Chris5692@users.noreply.github.com>
2020-04-05 16:56:25 +0530

    Default good

--- config.def.h
+++ config.def.h
@@ -9,10 +9,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#bbc5ff", "#282a36" },
+	[SchemeSel] = { "#282a36", "#82aaff" },
+	[SchemeSelHighlight] = { "#fff", "#82aaff" },
+	[SchemeNormHighlight] = { "#82aaff", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
